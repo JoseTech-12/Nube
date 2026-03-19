5d94d38c3b35f6323fd1f21871598e5914f25efc
JoseTech-12 <jp898467@gmail.com>
2026-03-19 18:31:48 -0500

    crearcion de formulario

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [id, setId] = useState("");
+  const [nombre, setNombre] = useState("");
 
   useEffect(() => {
     fetch("/api/getItems")
@@ -9,9 +12,43 @@ function App() {
       .then(data => setItems(data));
   }, []);
 
+  const handleRegistrar = () => {
+    fetch("/api/createItem", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ id, nombre })
+    })
+      .then(res => res.json())
+      .then(data => {
+        setItems([...items, data]);
+        setId("");
+        setNombre("");
+      });
+  };
+
+
   return (
     <div>
-      <h1>Items</h1>
+     
+      <h1>Usuarios</h1>
+       <div className="button">
+        <input 
+          type="number" 
+          placeholder="id" 
+          value={id} 
+          onChange={(e) => setId(e.target.value)} 
+        />
+        <input 
+          type="text" 
+          placeholder="Nombre" 
+          value={nombre} 
+          onChange={(e) => setNombre(e.target.value)} 
+        />
+        <button onClick={handleRegistrar} className="button"
+        >Registrar</button>
+      </div>
       {items.map(item => (
         <div key={item.id}>
           <p>{item.id}: {item.nombre}</p>
